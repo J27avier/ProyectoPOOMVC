3d2d354007447765d0e974e9f8d5864d62daec03
J27avier <salesortiz@gmail.com>
2019-05-02 20:48:51 -0500

    Controlador ingresar empleado terminado

--- src/Controlador/ContIngEmpleado.java
+++ src/Controlador/ContIngEmpleado.java
@@ -26,6 +26,7 @@ public class ContIngEmpleado implements ActionListener{
 		{
 			Insertar ins = new Insertar(conn);
 			Empleado emp = new Empleado();
+			int flag = 1;
 			//Validar Nombre
 			try {
 				String temp = pIngE.TNombre.getText().toString();
@@ -34,6 +35,7 @@ public class ContIngEmpleado implements ActionListener{
 				emp.setNombre(temp);
 			}
 			catch(Exception e1) {
+				flag=0;
 				pIngE.TNombre.setText("");
 				JOptionPane.showMessageDialog(pIngE, "Favor de ingresar un nombre valido");
 			}
@@ -45,6 +47,7 @@ public class ContIngEmpleado implements ActionListener{
 				emp.setAPaterno(temp);
 			}
 			catch(Exception e1) {
+				flag=0;
 				pIngE.TNombre.setText("");
 				JOptionPane.showMessageDialog(pIngE, "Favor de ingresar un apellido paterno valido");
 			}
@@ -56,12 +59,45 @@ public class ContIngEmpleado implements ActionListener{
 				emp.setAMaterno(temp);
 			}
 			catch(Exception e1) {
+				flag=0;
 				pIngE.TNombre.setText("");
 				JOptionPane.showMessageDialog(pIngE, "Favor de ingresar un apellido materno valido");
 			}
-			emp.setDir(pIngE.TDireccion.getText().toString());
-			emp.setDOB(Date.valueOf(String.valueOf(pIngE.dateChooser.getDate())));
+			//Validar direccion
+			if(pIngE.TDireccion.getText()!= null){
+				emp.setDir(pIngE.TDireccion.getText().toString());
+			}
+			else{
+				JOptionPane.showMessageDialog(pIngE, "Favor de introducir una direccion");
+				flag = 0;
+			}
+			//De java.util.Date a java.sql.Date, está más que insegurp si no es con dateChooser
+			if(pIngE.dateChooser.getDate() != null){
+				String DOB = String.format("%d-%d-%d", (1900 + pIngE.dateChooser.getDate().getYear()), (pIngE.dateChooser.getDate().getMonth()+1), pIngE.dateChooser.getDate().getDate());
+				emp.setDOB(java.sql.Date.valueOf(DOB));
+			}else{
+				JOptionPane.showMessageDialog(pIngE, "Favor de introducir una fecha");
+				flag = 0;
+			}
 
+			emp.setSexo(pIngE.DSexo.getSelectedItem().toString());
+			emp.setOcupacion(pIngE.DOcupacion.getSelectedItem().toString());
+			if(flag==0){
+				JOptionPane.showMessageDialog(pIngE, "Favor de corregir valores");
+			}
+			else{
+				int success = ins.iEmpleado(emp);
+				if(success==1)
+					JOptionPane.showMessageDialog(pIngE, "El empleado se agrego exitosamente");
+				else
+					JOptionPane.showMessageDialog(pIngE, "Hubo un error al agregar empleado");	
+				pIngE.TNombre.setText("");
+				pIngE.TAPaterno.setText("");
+				pIngE.TAMaterno.setText("");
+				pIngE.TDireccion.setText("");
+				pIngE.dateChooser.setDate(null);
+			}
+				 
 		}
 	}
 
