b1a455b0e0e7f770fc555b497a9c26a1feec7ab8
J27avier <salesortiz@gmail.com>
2019-05-02 19:58:40 -0500

    Minor changes

--- src/Controlador/ContIngEmpleado.java
+++ src/Controlador/ContIngEmpleado.java
@@ -2,9 +2,14 @@ package Controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Date;
 
 import Vista.*;
+import Modelo.*;
+
 import java.sql.Connection;
+
+import javax.swing.JOptionPane;
 
 
 public class ContIngEmpleado implements ActionListener{
@@ -19,8 +24,44 @@ public class ContIngEmpleado implements ActionListener{
 	public void actionPerformed(ActionEvent evento){
 		if(evento.getSource()==pIngE.ingresar)
 		{
-			//Insertar a BD
-			System.out.println("Ingresar!!!");
+			Insertar ins = new Insertar(conn);
+			Empleado emp = new Empleado();
+			//Validar Nombre
+			try {
+				String temp = pIngE.TNombre.getText().toString();
+				if(!temp.matches("^[a-zA-Z]+$"))
+					throw new Exception();
+				emp.setNombre(temp);
+			}
+			catch(Exception e1) {
+				pIngE.TNombre.setText("");
+				JOptionPane.showMessageDialog(pIngE, "Favor de ingresar un nombre valido");
+			}
+			//Validar APaterno
+			try {
+				String temp = pIngE.TAPaterno.getText().toString();
+				if(!temp.matches("^[a-zA-Z]+$"))
+					throw new Exception();
+				emp.setAPaterno(temp);
+			}
+			catch(Exception e1) {
+				pIngE.TNombre.setText("");
+				JOptionPane.showMessageDialog(pIngE, "Favor de ingresar un apellido paterno valido");
+			}
+			//Validar AMaterno
+			try {
+				String temp = pIngE.TAMaterno.getText().toString();
+				if(!temp.matches("^[a-zA-Z]+$"))
+					throw new Exception();
+				emp.setAMaterno(temp);
+			}
+			catch(Exception e1) {
+				pIngE.TNombre.setText("");
+				JOptionPane.showMessageDialog(pIngE, "Favor de ingresar un apellido materno valido");
+			}
+			emp.setDir(pIngE.TDireccion.getText().toString());
+			emp.setDOB(Date.valueOf(String.valueOf(pIngE.dateChooser.getDate())));
+
 		}
 	}
 
