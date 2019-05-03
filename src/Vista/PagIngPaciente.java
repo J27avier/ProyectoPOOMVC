7057f70c4d0781d3210cfdd6b35cb92b064df43b
alexz1200 <alexzego@hotmail.com>
2019-05-02 19:31:02 -0500

    cambios

--- src/Vista/PagIngPaciente.java
+++ src/Vista/PagIngPaciente.java
@@ -5,24 +5,40 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
+
+
+
 public class PagIngPaciente extends JFrame
 {
-	
-	public void init()
+	public Container cp;
+	public PagIngEmpleado()
 	{
-	  JTabbedPane pestaña=new JTabbedPane();
-	  JPanel p1=new JPanel();
-	  JLabel l1=new JLabel("Este es el panel uno");
-	  p1.add(l1);
-	  JPanel p2=new JPanel();
-	  JLabel l2=new JLabel("Este es el panel dos");
-	  p2.add(l2);
-	  JPanel p3=new JPanel();
-	  JLabel l3=new JLabel("Este es el panel tres");
-	  p3.add(l3);
-	  pestaña.addTab("Pestana Uno", p1);
-	  pestaña.addTab("Pestana Dos", p2);
-	  pestaña.addTab("Pestana Tres", p3);
-	  add(pestaña);
+		super("Dar de alta un empleado");
+		setSize(1000,500);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setResizable(true);
+
+
+		JPanel menuPrincipal = new JPanel();
+		menuPrincipal.setLayout(null);
+
+
+		JTabbedPane pestaña=new JTabbedPane();
+		JPanel p1=new JPanel();
+		JLabel l1=new JLabel("Este es el panel uno");
+		p1.add(l1);
+		JPanel p2=new JPanel();
+		JLabel l2=new JLabel("Este es el panel dos");
+		p2.add(l2);
+		JPanel p3=new JPanel();
+		JLabel l3=new JLabel("Este es el panel tres");
+		p3.add(l3);
+		pestaña.addTab("Pestana Uno", p1);
+		pestaña.addTab("Pestana Dos", p2);
+		pestaña.addTab("Pestana Tres", p3);
+		add(pestaña);
 	}
+
+
+
 }
